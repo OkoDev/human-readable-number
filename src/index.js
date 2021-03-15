4c9d93fb56663d8a4401383836f407e51e02ceca
okodev <oko.webdev@gmail.com>
2021-03-15 05:36:36 +0300

    feat: add solution to src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(num) {
+
+    const a = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', 'ten ', 'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ', 'eighteen ', 'nineteen '];
+    const b = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    let n = ('000' + num)
+        .substr(-3)
+        .match(/^(\d{1})(\d{2})$/)
+    ;
+    let str = '';
+    str += (n[0] != 0) ? '' : 'zero';
+    str += (n[1] != 0) ? a[Number(n[1])]  + 'hundred ' : '';
+    str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) : '';
+    return str.trimRight();
+};
